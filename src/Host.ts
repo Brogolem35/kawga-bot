7ffc107804e5fa01448b9786eba8b2012af4870c
Brogolem35 <brogolem35@protonmail.com>
2023-06-16 14:13:03 +0300

    Convert Host.ts

--- src/Host.ts
+++ src/Host.ts
@@ -1,6 +1,12 @@
+import { Message, Snowflake } from "discord.js";
+
 export class Host
 {
-	constructor(_id, _message)
+	id: Snowflake;
+	message: Message;
+	joined: Boolean;
+	
+	constructor(_id: string, _message: Message<boolean>)
 	{
 		this.id = _id;
 		this.message = _message;
