962741de3f59326f646769e04a4c86bca68dc536
Brogolem35 <brogolem35@protonmail.com>
2023-07-03 07:09:20 +0300

    Fix import

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,6 @@
 import {APIEmbed, Client, EmbedBuilder, Emoji, Message, TextChannel, User} from "discord.js";
 
-import {HOST_LIFESPAN, REMOVE_UNWANTED_REACTIONS, UPDATE_JOIN} from "./config";
+import {HOST_LIFESPAN, REMOVE_UNWANTED_REACTIONS, UPDATE_JOIN} from "./config.js";
 import {Host} from "./Host";
 
 export function updateHosts(client: Client, hostMap: Map<string, Host>, joinEmoji: Emoji)
