4ee1c76fd7c4175542b867fe9bb4e3960bf7e6c0
Brogolem35 <brogolem35@protonmail.com>
2023-07-05 08:50:02 +0300

    Add joinDate

--- src/Host.ts
+++ src/Host.ts
@@ -5,11 +5,19 @@ export class Host
 	id: string;
 	message: Message;
 	joined: Boolean;
+	joinDate: number|null; // The date that the "joined" is set to true, in Unix time
 
-	constructor(_id: string, _message: Message<boolean>)
+	public constructor(_id: string, _message: Message<boolean>)
 	{
 		this.id = _id;
 		this.message = _message;
 		this.joined = false;
+		this.joinDate = null;
+	}
+
+	public join()
+	{
+		this.joined = true;
+		this.joinDate = Date.now();
 	}
 }
